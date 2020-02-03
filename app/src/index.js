1eb74cfa52236dbf0b1def948a724703f04f26e7
Dominik Flisak <d.flisak@gmail.com>
2020-02-04 00:36:22 +0100

    React to account change event in MetaMask

--- app/src/index.js
+++ app/src/index.js
@@ -20,10 +20,14 @@ const App = {
 
       // get accounts
       const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
+      this.setDefaultAccount(accounts[0]);
     } catch (error) {
       console.error("Could not connect to contract or chain.");
     }
+  },
+
+  setDefaultAccount: function(account) {
+    this.account = account;
   },
 
   setStatus: function(message) {
@@ -59,10 +63,15 @@ const App = {
 window.App = App;
 
 window.addEventListener("load", async function() {
-  if (window.ethereum) {
+  const { ethereum } = window;
+  if (ethereum) {
     // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
+    App.web3 = new Web3(ethereum);
+    await ethereum.enable(); // get permission to access accounts
+
+    ethereum.on('accountsChanged', (accounts) => {
+      App.setDefaultAccount(accounts[0]);
+    });
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
